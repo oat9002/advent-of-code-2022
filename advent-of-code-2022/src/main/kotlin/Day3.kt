ea7ffaaf5cd14b94200bd75bc5c7825110c9ea53
oat9002 <oat9002@hotmail.com>
2022-12-03 23:09:19 +0700

    day3 p1

--- advent-of-code-2022/src/main/kotlin/Day3.kt
+++ advent-of-code-2022/src/main/kotlin/Day3.kt
@@ -13,18 +13,22 @@ fun charToInt(c: Char): Int {
 fun findShareItemPriority(input: String): Int {
     val r1 = input.substring(0, input.length / 2)
     val r2 = input.substring(input.length / 2, input.length)
-    val repeated = HashSet<Char>()
+    val repeated = HashMap<Char, Boolean>()
+    var result = 0
 
-    r1.forEach { f ->
-        if (!repeated.contains(f)) {
-            val dup = r2.find { s -> s.equals(f) }
-            if (dup != null) {
-                repeated.add(dup)
+    r1.forEach { repeated.put(it, false) }
+    r2.forEach {
+        val dup = repeated.get(it)
+
+        if (dup != null) {
+            if (!dup) {
+                result += charToInt(it)
+                repeated.put(it, true)
             }
         }
     }
 
-    return repeated.toList().sumOf { charToInt(it) }
+    return result
 }
 
 fun day3part1() {
