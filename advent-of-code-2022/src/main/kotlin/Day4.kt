19b690f9b21e836d0557868acff6b7fae08d064c
oat9002 <oat9002@hotmail.com>
2022-12-05 09:41:20 +0700

    day4 p2

--- advent-of-code-2022/src/main/kotlin/Day4.kt
+++ advent-of-code-2022/src/main/kotlin/Day4.kt
@@ -1,13 +1,14 @@
 fun main() {
     Day4.part1()
+    Day4.part2()
 }
 
 object Day4 {
-    fun isFullyDuplicate(input: String): Boolean {
+    fun isDuplicate(input: String, isFullyDuplicate: Boolean): Boolean {
         val elfs = input.split(",")
         val firstElf = elfs[0].split("-").map { it.toInt() }
         val secondElf = elfs[1].split("-").map { it.toInt() }
-        val compare = { x1: Int, x2: Int, y1: Int, y2: Int -> x1 >= y1 && x2 <= y2 }
+        val compare = { x1: Int, x2: Int, y1: Int, y2: Int -> if (isFullyDuplicate) x1 >= y1 && x2 <= y2 else  x2 >= y1 && y2 >= x1 }
 
         if (compare(firstElf[0], firstElf[1], secondElf[0], secondElf[1]) || compare(secondElf[0], secondElf[1], firstElf[0], firstElf[1])) {
             return true
@@ -18,7 +19,14 @@ object Day4 {
 
     fun part1() {
         val input = object{}::class.java.getResource("day4_1.txt")?.readText(Charsets.UTF_8).orEmpty().split("\n")
-        val result = input.map { isFullyDuplicate(it) }.foldRight(0) { isFullyDup, count -> if (isFullyDup)  count + 1 else count }
+        val result = input.map { isDuplicate(it, true) }.foldRight(0) { isFullyDup, count -> if (isFullyDup)  count + 1 else count }
+
+        println(result)
+    }
+
+    fun part2() {
+        val input = object{}::class.java.getResource("day4_2.txt")?.readText(Charsets.UTF_8).orEmpty().split("\n")
+        val result = input.map { isDuplicate(it, false) }.foldRight(0) { isPartialOrFull, count -> if (isPartialOrFull)  count + 1 else count }
 
         println(result)
     }
