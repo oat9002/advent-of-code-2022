2cef84b7e28a898dd8866ab254197e9371cc35f6
oat9002 <oat9002@hotmail.com>
2022-12-15 01:10:51 +0700

    day8 p2

--- advent-of-code-2022/src/main/kotlin/Day8.kt
+++ advent-of-code-2022/src/main/kotlin/Day8.kt
@@ -2,6 +2,7 @@ import kotlin.math.sign
 
 fun main() {
     Day8.part1()
+    Day8.part2()
 }
 
 object Day8 {
@@ -32,5 +33,69 @@ object Day8 {
         }
 
         println(result)
+    }
+
+    fun part2() {
+        val input = object{}::class.java.getResource("day8_2.txt")?.readText(Charsets.UTF_8).orEmpty()
+        val grid = getGrid(input)
+        var result = 0
+        var a = ""
+
+        grid.forEachIndexed{ rowIdx, row ->
+            row.forEachIndexed { colIdx, col ->
+                if (rowIdx != 0 && rowIdx != grid.size - 1 && colIdx != 0 && colIdx != row.size - 1) {
+                    val left = (0 until colIdx).toList().foldRight(Pair(0, false)){ i, acc ->
+                        if (acc.second) {
+                            acc
+                        } else if (col ==  grid[rowIdx][i]) {
+                            Pair(acc.first + 1, true)
+                        } else if (col > grid[rowIdx][i]) {
+                            Pair(acc.first + 1, acc.second)
+                        } else {
+                            Pair(acc.first, true)
+                        }
+                    }
+                    val right = (colIdx + 1 until row.size).toList().fold(Pair(0, false)){ acc, i ->
+                        if (acc.second) {
+                            acc
+                        } else if(col == grid[rowIdx][i]) {
+                            Pair(acc.first + 1, true)
+                        } else if (col > grid[rowIdx][i]) {
+                            Pair(acc.first + 1, acc.second)
+                        } else {
+                            Pair(acc.first, true)
+                        }
+                    }
+                    val top = (0 until rowIdx).toList().foldRight(Pair(0, false)){ i, acc ->
+                        if (acc.second) {
+                            acc
+                        } else if (col == grid[i][colIdx]) {
+                            Pair(acc.first + 1, true)
+                        } else if (col > grid[i][colIdx]) {
+                            Pair(acc.first + 1, acc.second)
+                        } else {
+                            Pair(acc.first, true)
+                        }
+                    }
+                    val bottom = (rowIdx + 1 until grid.size).toList().fold(Pair(0, false)){ acc, i ->
+                        if (acc.second) {
+                          acc
+                        } else if (col == grid[i][colIdx]) {
+                            Pair(acc.first + 1, true)
+                        } else if (col > grid[i][colIdx]) {
+                            Pair(acc.first + 1, acc.second)
+                        } else {
+                            Pair(acc.first, true)
+                        }
+                    }
+                    val sceneScore = left.first * right.first * top.first * bottom.first
+
+                    result = if(sceneScore > result) sceneScore else result
+                }
+            }
+        }
+
+        println(result)
+        println(a)
     }
 }
