e5343132fcbdf88b40c2cdc49fc54eedb42ca323
oat9002 <oat9002@hotmail.com>
2022-12-29 15:33:26 +0700

    day10 p2

--- advent-of-code-2022/src/main/kotlin/Day10.kt
+++ advent-of-code-2022/src/main/kotlin/Day10.kt
@@ -1,5 +1,6 @@
 fun main() {
     Day10.part1()
+    Day10.part2()
 }
 
 object Day10 {
@@ -41,5 +42,44 @@ object Day10 {
         }
 
         println(getSumOfSignalStrength(outputList))
+    }
+
+    fun part2() {
+        val input = object{}::class.java.getResource("day10_2.txt")?.readText(Charsets.UTF_8).orEmpty().split("\n")
+        val outputList = arrayListOf<String>()
+        var spritePos = 0
+        var crtPos = 0
+        val cycleSprit = 40
+
+        for (i in input) {
+            val (usedCycle, number) = parseCommand(i)
+
+            for (t in 0 until usedCycle) {
+                if (crtPos >= spritePos && crtPos <=spritePos + 2) {
+                    outputList.add("#")
+                } else {
+                    outputList.add(".")
+                }
+
+                crtPos++
+
+                if (crtPos == cycleSprit) {
+                    crtPos = 0
+                }
+            }
+
+            spritePos += number
+        }
+
+        val width = 40
+        var pos = 0
+        while(pos < 240) {
+
+            for (i in pos until pos + width) {
+                print(outputList[i])
+            }
+            println()
+            pos += width
+        }
     }
 }
